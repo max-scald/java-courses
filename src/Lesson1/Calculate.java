0b6a25c4f797e53960a357e6dd1b021445fc3dfc
Scald <max-scald@mail.ru>
2016-08-29 17:21:58 +0300

    Second commit

--- src/Lesson1/Calculate.java
+++ src/Lesson1/Calculate.java
@@ -6,5 +6,10 @@ package Lesson1;
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Calculate...");
+        int first = 1;
+        int second = 2;
+        int summ = first + second;
+        System.out.println("Summ " + summ);
+
     }
 }
