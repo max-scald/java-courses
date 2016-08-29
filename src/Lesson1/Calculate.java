81d2b138a4c6fdf8708e1884155d0d549af74199
Scald <max-scald@mail.ru>
2016-08-29 17:09:55 +0300

    First commit

---
+++ src/Lesson1/Calculate.java
@@ -0,0 +1,10 @@
+package Lesson1;
+
+/**
+ * Created by Scald on 29.08.2016.
+ */
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("Calculate...");
+    }
+}
